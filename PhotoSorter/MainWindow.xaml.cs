374009ae3486b59d096e82854e69688e0a510bda
dylanArleTools <dylanarle@gmail.com>
2024-02-22 13:53:21 -0700

    remove unneeded usings

--- PhotoSorter/MainWindow.xaml.cs
+++ PhotoSorter/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Forms;
+﻿using System.Windows;
 using System.Windows.Input;
 
 namespace PhotoSorter
